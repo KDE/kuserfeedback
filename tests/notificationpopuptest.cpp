4365208adafd5aea7f51e37a927e20f2ceb35d97
Volker Krause <vkrause@kde.org>
2017-02-25 13:39:04 +0100

    Add notification popup, as a smaller replacement for the old widget

---
+++ tests/notificationpopuptest.cpp
@@ -0,0 +1,62 @@
+/*
+    Copyright (C) 2017 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <provider/widgets/notificationpopup.h>
+#include <provider/core/provider.h>
+#include <provider/core/surveyinfo.h>
+
+#include <QApplication>
+#include <QPushButton>
+#include <QStandardPaths>
+#include <QVBoxLayout>
+
+using namespace UserFeedback;
+
+int main(int argc, char **argv)
+{
+    QStandardPaths::setTestModeEnabled(true);
+
+    QCoreApplication::setApplicationName(QStringLiteral("notificationpopuptest"));
+    QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
+    QCoreApplication::setOrganizationDomain(QStringLiteral("kde.org"));
+    QCoreApplication::setApplicationVersion(QStringLiteral("1984"));
+
+    QApplication app(argc, argv);
+
+    Provider provider;
+
+    QWidget topLevel;
+    auto layout = new QVBoxLayout(&topLevel);
+    auto button = new QPushButton(QStringLiteral("Show Encouragement"));
+    layout->addWidget(button);
+    QObject::connect(button, &QPushButton::clicked, &provider, &Provider::showEncouragementMessage);
+    button = new QPushButton(QStringLiteral("New Survey Available"));
+    layout->addWidget(button);
+    QObject::connect(button, &QPushButton::clicked, &provider, [&provider]() {
+        SurveyInfo info;
+        info.setId(42);
+        info.setUrl(QUrl(QStringLiteral("https://www.kde.org/")));
+        emit provider.surveyAvailable(info);
+    });
+
+    auto popup = new NotificationPopup(&topLevel);
+    popup->setFeedbackProvider(&provider);
+
+    topLevel.resize(1024, 786);
+    topLevel.show();
+    return app.exec();
+}

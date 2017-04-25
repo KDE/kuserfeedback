33ae0a2623029c48fd67b99926db17db7c4e1e10
Aleix Pol <aleixpol@kde.org>
2017-04-25 12:59:10 +0200

    Fix build with Qt5.9
    Reviewers: vkrause
    
    Reviewed By: vkrause
    
    Differential Revision: https://phabricator.kde.org/D5578

--- 3rdparty/qt/modeltest.cpp
+++ 3rdparty/qt/modeltest.cpp
@@ -438,7 +438,7 @@ void ModelTest::data()
     // Check that the alignment is one we know about
     QVariant textAlignmentVariant = model->data ( model->index ( 0, 0 ), Qt::TextAlignmentRole );
     if ( textAlignmentVariant.isValid() ) {
-        int alignment = textAlignmentVariant.toInt();
+        uint alignment = textAlignmentVariant.toUInt();
         QCOMPARE( alignment, ( alignment & ( Qt::AlignHorizontal_Mask | Qt::AlignVertical_Mask ) ) );
     }
 

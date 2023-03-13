a48d3763f7ac6295bf8aed6c74ff1fa21c0526b9
Luigi Toscano <luigi.toscano@tiscali.it>
2023-03-13 23:01:13 +0100

    Fix the translation template number (5->6)
    KF5 and KF6 must be co-installable, so change the major version
    number integrated into the translation template name.
    
    Even though kuserfeedback was technically not part of Frameworks 5.x,
    as it was used quite a while, having it co-installable
    with its KF6 counterparts can be useful.

--- src/provider/Messages.sh
+++ src/provider/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/userfeedbackprovider5_qt.pot
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/userfeedbackprovider6_qt.pot
 
